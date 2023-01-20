432a663ddf17a26b2fc78d4a9d9e1da4b8c6a3fb
pablomaxia <pablo.maxia.rodriguez.alu@iesjulioverne.es>
2023-01-20 10:16:28 +0100

    calculadora

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,16 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Calculadora from "./components/Calculadora";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Calculadora />
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Calculadora />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- calculadora/src/components/Calculadora.js
+++ calculadora/src/components/Calculadora.js
@@ -1,27 +1,97 @@
 import { Component } from "react";
-import "../App.css";
+import "../css/components.css";
 import ReactDOM from "react-dom";
 export default class Calculadora extends Component {
   constructor(props) {
     super(props);
-    this.state = { resultado: 0 };
+    this.state = { resultado: "" };
     this.limpiarPantalla = this.limpiarPantalla.bind(this);
   }
 
   limpiarPantalla() {
     this.setState((state) => ({
-      resultado: 1,
+      resultado: "",
     }));
   }
 
   render() {
     return (
       <div>
+        <h1 className="">Calculadora</h1>
         <form>
-          <div className="resultado">{this.state.resultado}</div>
-          <button className="ce" onClick={this.limpiarPantalla}>
-            CE
-          </button>
+          <table className="table">
+            <tbody>
+              <tr>
+                <td className="resultado">{this.state.resultado}</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                  <button className="ce" onClick={this.limpiarPantalla}>
+                    CE
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className="">1</button>
+                </td>
+                <td>
+                  <button className="">2</button>
+                </td>
+                <td>
+                  <button className="">3</button>
+                </td>
+                <td>
+                  <button className="">/</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className="">4</button>
+                </td>
+                <td>
+                  <button className="">5</button>
+                </td>
+                <td>
+                  <button className="">6</button>
+                </td>
+                <td>
+                  <button className="">x</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className="">7</button>
+                </td>
+                <td>
+                  <button className="">8</button>
+                </td>
+                <td>
+                  <button className="">9</button>
+                </td>
+                <td>
+                  <button className="">-</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className="">.</button>
+                </td>
+                <td>
+                  <button className="">0</button>
+                </td>
+                <td>
+                  <button className="">=</button>
+                </td>
+                <td>
+                  <button className="">+</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </form>
       </div>
     );
