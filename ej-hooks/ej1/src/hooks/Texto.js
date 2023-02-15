3a0812da78d7afc74a4948c812998d211f7a0777
pablomaxia <pablo.maxia.rodriguez.alu@ies.julioverne.es>
2023-02-16 00:21:12 +0100

    [ej hooks] creado ej5

--- ej-hooks/ej1/src/hooks/Texto.js
+++ ej-hooks/ej1/src/hooks/Texto.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-const Texto = ({ nombre = "", tipo = "", valor = 0, metodo = () => null }) => (
-  <div className="container">
-    <input type={tipo} value={valor} onChange={metodo} name={nombre}></input>
-=======
 const Texto = ({
   nombre = "",
   tipo = "",
   valor = 0,
-  metodoOnChange = () => null,
+  metodoOnChange = () => null
 }) => (
   <div className="container">
     <input
@@ -15,7 +10,6 @@ const Texto = ({
       type={tipo}
       value={valor}
       onChange={metodoOnChange}></input>
->>>>>>> 387e920dc8ce437cc57082e154f56a165e38d68e
   </div>
 );
 export default Texto;
