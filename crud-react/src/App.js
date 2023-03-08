ad173920732b2badd22b91fd5f96df1d3d029577
pablomaxia <pablo.maxia.rodriguez.alu@ies.julioverne.es>
2023-03-08 21:32:12 +0100

    [ crud react ] se muestra la lista de productos

--- crud-react/src/App.js
+++ crud-react/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    productos: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch("/producto");
+    const body = await response.json();
+    this.setState({ productos: body });
+  }
+
+  render() {
+    const { productos } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-intro">
+            <h2>Productos</h2>
+            {productos.map((producto) => (
+              <div key={producto.id}>
+                {producto.nombre} {producto.precio}
+                {producto.categoria}
+              </div>
+            ))}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
