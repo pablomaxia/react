6af744ddc6f5631ff0e0b0914f7f38fa098d5609
pablomaxia <pablo.maxia.rodriguez.alu@ies.julioverne.es>
2023-03-09 00:06:26 +0100

    [ crud-react ] falta editar y añadir

--- crud-react/src/App.js
+++ crud-react/src/App.js
@@ -1,34 +1,22 @@
-import "./App.css";
 import React, { Component } from "react";
+import "./App.css";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProductoList from "./components/ProductoList";
+import ProductoEdit from "./components/ProductoEdit";
 
 class App extends Component {
-  state = {
-    productos: []
-  };
-
-  async componentDidMount() {
-    const response = await fetch("/producto");
-    const body = await response.json();
-    this.setState({ productos: body });
-  }
-
   render() {
-    const { productos } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <div className="App-intro">
-            <h2>Productos</h2>
-            {productos.map((producto) => (
-              <div key={producto.id}>
-                {producto.nombre} {producto.precio}
-                {producto.categoria}
-              </div>
-            ))}
-          </div>
-        </header>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/producto" exact={true} component={ProductoList} />
+          <Route path="/producto/:id" component={ProductoEdit} />
+        </Switch>
+      </Router>
     );
   }
 }
+
 export default App;
